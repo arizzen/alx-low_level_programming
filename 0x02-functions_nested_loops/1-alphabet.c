48b01a0d13e86834c8953819872dac66a682e13e
arizzen <a1.arizzen@gmail.com>
2022-10-19 13:19:41 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alpha in lowercase
+ */
+void print_alphabet(void)
+{
+	char zz;
+
+	for (zz = 'a'; zz <= 'z'; zz++)
+		_putchar(zz);
+	_putchar('\n');
+}
